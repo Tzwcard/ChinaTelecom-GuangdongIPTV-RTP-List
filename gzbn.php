c49d05ee7e790ba1d88a26872f44789cc41c936a
Tzwcard <Tzwcard@users.noreply.github.com>
2020-06-18 19:47:09 +0800

    fixed function name

--- gzbn.php
+++ gzbn.php
@@ -1,5 +1,5 @@
 <?php
-function _curl_get_with_proxy($url) {
+function _curl_get($url) {
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL,$url);
@@ -19,7 +19,7 @@ function _curl_get_with_proxy($url) {
 function _get_gzbn_broadcast_list() {
   $prog_list = 'https://channel.gztv.com/channelf/site/rest/tv-channel/getChannelAndProgramList';
   
-  $prog_list_data = json_decode(_curl_get_with_proxy($prog_list), 1);
+  $prog_list_data = json_decode(_curl_get($prog_list), 1);
   $channels = [];
 
   foreach ($prog_list_data['data'] ?? [] as $v) {
@@ -36,18 +36,13 @@ function _jump_gzbn_broadcast_m3u8($channel) {
   $video_page_api = 'https://channel.gztv.com/channelf/viewapi/player/channelVideo?id=%s&commentFrontUrl=https://comment.gztv.com/commentf';
   
   $video_page_url = sprintf($video_page_api, $channel['uuid']);
-  $video_page_data = _curl_get_with_proxy($video_page_url);
+  $video_page_data = _curl_get($video_page_url);
 
   $m3u8_uri = NULL; $secondid = NULL;
   if (preg_match('/standardUrl=\'(.*)\';/', $video_page_data, $matches)) $m3u8_uri = $matches[1];
   if (preg_match('/secondId=\'(.*)\';/', $video_page_data, $matches)) $secondid = $matches[1];
 
   header("Location: " . $m3u8_uri);
-}
-
-if (isset($_GET['all_channels']) && intval($_GET['all_channels']) == 1) {
-  $channels = _get_gzbn_broadcast_list();
-  return ;
 }
 
 $all_channels = _get_gzbn_broadcast_list();
